5d1a62cf2085449971df31e684058f39ad6fb970
Arnaud <arlecomt@student.42.fr>
2020-02-08 18:38:22 +0100

    running java binary in cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include <fstream>
+#include <cstdlib>
 #include <iostream>
 
 int main() {
-    std::cout << "Bonjour, Monde!" << std::endl;
+    std::system("java -cp ./deps/7-Wonders-Simulator/bin controller.Controller");
     return 0;
 }
